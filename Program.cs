8bd48f03c438a55c3332329e03dd837045f1915e
isabele_pinto <isabelepfso@gmail.com>
2022-03-07 11:57:56 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Isabele", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWhitdrawal(120,DateTime.Now,"Hammock"); 
+
+            account.MakeWhitdrawal(50,DateTime.Now,"Xbox Game");
+
+            Console.WriteLine(account.GetAccountHistory());
+            
+            
+            
+            
+            
+            
+            
+            
+            // Test that the initial balances must be positive.
+            // BankAccount invalidAccount;
+            // try
+            // {
+            //     invalidAccount = new BankAccount("invalid", -55);
+            // }
+            // catch (ArgumentOutOfRangeException e)
+            // {
+            //     Console.WriteLine("Exception caught creating account with negative balance");
+            //     Console.WriteLine(e.ToString());
+            //     return;
+            // }
+
+            
+        }
+    }
+}
